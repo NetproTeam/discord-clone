86dbb507f67010351b7da4fae090d68e50f756e0
유수현 <youg4231998@naver.com>
2024-05-29 17:08:36 +0900

    채팅방 참가 버튼

--- src/components/home/ChannelList.jsx
+++ src/components/home/ChannelList.jsx
@@ -1,11 +1,16 @@
-import React from "react";
+import React, {useState} from "react";
 import SoundIcon from "@mui/icons-material/VolumeUp"
+import { colors } from "@mui/material";
 
 function ListHeader(props) {
+
     return (
         <div className="list-header">
-            <SoundIcon/>
-            <h4>{props.channelName}</h4>
+            <button onClick={props.addUser} style={{ display: 'flex', alignItems: 'center', padding: '1px', border: 'none', background: 'none', cursor: 'pointer' }}>
+                <SoundIcon style={{color: "white"}} />
+                <h4 style={{ marginLeft: '10px', color: "white" }}>
+                    {props.channelName}</h4>
+            </button>
         </div>
     );
 }
@@ -19,10 +24,19 @@ function User(props) {
 }
 
 function ChannelList(props) {
+    const [showUser, setShowUser] = useState(false);
+
+    const addUser = () => {
+        setShowUser(!showUser);
+    };
     return (
         <div className="channel-detail">
-            <ListHeader channelName={props.channelName}/>
-            <User username={props.username}/>
+            <ListHeader channelName={props.channelName} addUser={addUser}/>
+            
+            {props.users.map( user =>
+                <User key={user.index} username={user.name}/>
+            )}
+                
         </div>
     );
 }
